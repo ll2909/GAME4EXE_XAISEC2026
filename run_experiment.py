b8a06975e1a635764a7b3c17c3110948fd0bba50
Luca Lobascio <48275371+ll2909@users.noreply.github.com>
2026-04-13 11:32:21 +0200

    Add files via upload

--- run_experiment.py
+++ run_experiment.py
@@ -1,11 +1,13 @@
 from configparser import ConfigParser
 from generate_adversarials import generate_adversarials
 from transferability_test import transferability_test
+import sys
 
 if __name__ == "__main__":
-    
+    path = sys.argv[1]
+    print("Config path: ", path)
     cfg_parser = ConfigParser()
-    cfg_parser.read("config.conf")
+    cfg_parser.read(path)
 
     config = dict(cfg_parser["CONFIG"])
 
